e3a88f6599ea02714e89b0447ca3f7abe2b77b80
Yujun Liu <yujunliu150@gmail.com>
2024-10-31 22:55:12 -0400

    minor change

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -4,17 +4,17 @@ import projectThree from "../assets/project-3.png";
 
 const projects = {
     1: {
-        title: "SoMe Platform", 
+        title: "Flexbench", 
         image: projectOne,
         description: (
             <>
                 <p>
-                    asdfasdfasdfasdfdsafasdfasfasdfasdfsadfasdfasdf
+                Flexbench is a fully customizable NodeJS script , generating simulated HTTP traffic. It can be used as a standalone script, as a desktop-app and as a server to simulate traffic with specific characteristics, such as read/write ratio, duration, number of requests to generate, in/out traffic throttling and more.
                 </p>
             </>
         ),
-        github: "https://github.com",
-        demo: "https://netlify.com",
+        github: "https://github.com/flexivian/flexbench",
+        demo: "https://flexivian.github.io/flexbench/",
     },
     2: {
         title: "Productivity App", 
@@ -30,7 +30,7 @@ const projects = {
         demo: "https://netlify.com",
     },
     3: {
-        title: "Food Delivery App", 
+        title: "Mock Website Apps", 
         image: projectThree,
         description: (
             <>
@@ -39,8 +39,8 @@ const projects = {
                 </p>
             </>
         ),
-        github: "https://github.com",
-        demo: "https://netlify.com",
+        github: "https://github.com/AJun01?tab=repositories",
+        demo: "https://github.com/AJun01?tab=repositories",
     },
 }
 
